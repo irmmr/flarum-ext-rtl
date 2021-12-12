8d2bce01700997be038d0a236458a06920737d87
irmmr <irmmr.ir@gmail.com>
2021-12-12 04:11:42 +0330

    change navigation back icon

--- js/src/forum/index.js
+++ js/src/forum/index.js
@@ -1,6 +1,8 @@
-import changePaneShowX from "./changePaneShowX"
+import changePaneShowX from './changePaneShowX'
+import changeNavBackBtn from './changeNavBackBtn'
 import app from 'flarum/app'
 
 app.initializers.add('irmmr-flarum-rtl-ext', () => {
     changePaneShowX()
+    changeNavBackBtn()
 })
